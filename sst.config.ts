1b60fb81d8cef7d34410b78d5047a6ab6fea1bc3
albsbz <alexkamens.d@gmail.com>
2023-10-09 21:19:11 +0300

    next downgrade

--- sst.config.ts
+++ sst.config.ts
@@ -1,20 +1,24 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { SSTConfig } from 'sst';
+import { NextjsSite } from 'sst/constructs';
 
 export default {
-  config(_input) {
-    return {
-      name: "sst-my-app",
-      region: "us-east-1",
-    };
-  },
-  stacks(app) {
-    app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+	config(_input) {
+		return {
+			name: 'sst-my-app-v1',
+			region: 'us-east-1',
+			ssmPrefix: "/albsbz/sst/",
+		};
+	},
+	stacks(app) {
+		app.stack(function Site({ stack }) {
+			const site = new NextjsSite(stack, 'site');
+			if (app.stage !== 'prod') {
+				app.setDefaultRemovalPolicy('destroy');
+			}
 
-      stack.addOutputs({
-        SiteUrl: site.url,
-      });
-    });
-  },
+			stack.addOutputs({
+				SiteUrl: site.url,
+			});
+		});
+	},
 } satisfies SSTConfig;
